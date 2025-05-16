88d60172fa8a68f1ab0722a2ad48fe37976e5f2e
Rajdeep Singh <rjdprocks9977@gmail.com>
2025-05-17 04:35:36 +0530

    update vite output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DriftNotes/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/DriftNotes/',
+  build: {
+    outDir: 'dist' // Optional, as 'dist' is default
+  }
 });
